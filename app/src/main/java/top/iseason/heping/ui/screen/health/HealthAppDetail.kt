a35751369aaa8882c498213338d140832071054c
Iseason2000 <Iseason2000@qq.com>
2021-12-23 00:52:14 +0800

    完善细节

--- app/src/main/java/top/iseason/heping/ui/screen/health/HealthAppDetail.kt
+++ app/src/main/java/top/iseason/heping/ui/screen/health/HealthAppDetail.kt
@@ -1,11 +1,14 @@
 package top.iseason.heping.ui.screen.health
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import top.iseason.heping.model.ModelManager
 
 @Composable
 fun HealthAppDetail(packageName: String) {
-
+    Box(modifier = Modifier.fillMaxSize())
     val appInfoForAllDays = ModelManager.getViewModel().getAppInfoForAllDays(packageName)
 
 }
